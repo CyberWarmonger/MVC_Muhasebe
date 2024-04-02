951963a5bc62a12b4787a30c9bd464b941919a7d
CyberWarmonger <CyberWarmonger@CYBERWARMONGER>
2024-04-02 15:56:59 +0300

    Add project files.

---
+++ Controllers/KategoriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Stok.Models.Entity;
+
+namespace MVC_Stok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORILER.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ Controllers/UrunController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Stok.Models.Entity;
+
+namespace MVC_Stok.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler=db.TBLURUNLER.ToList();
+            return View(degerler);
+        }
+    }
+}
