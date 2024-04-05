ed577573e2f9ec11dcc12c10afbc54e9ea6ea178
CyberWarmonger <CyberWarmonger@CYBERWARMONGER>
2024-04-05 14:05:26 +0300

    Validation kontrolleri eklendi. Ürün Güncelleme Yapıldı.

--- Models/Entity/TBLKATEGORILER.cs
+++ Models/Entity/TBLKATEGORILER.cs
@@ -22,7 +22,7 @@ namespace MVC_Stok.Models.Entity
         }
     
         public short KATEGORIID { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Kategori Adini Giriniz...!")]
         public string KATEGORIAD { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
